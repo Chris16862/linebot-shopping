90a555c07ffdc5262c5e13fded675e1c4d89f602
unknown <>
2017-09-26 19:38:55 +0800

    test

--- SHOP.py
+++ SHOP.py
@@ -1,9 +1,6 @@
-# -*- coding: utf-8 -*-
-
-#  Licensed under the Apache License, Version 2.0 (the "License"); you may
-#  not use this file except in compliance with the License. You may obtain
 from __future__ import unicode_literals
 
+import jieba
 import os
 import sys
 from argparse import ArgumentParser
@@ -20,8 +17,8 @@ from linebot.models import (
 )
 
 app = Flask(__name__)
+jieba.load_userdict('moe.dict')
 
-# get channel_secret and channel_access_token from your environment variable
 channel_secret = os.getenv('LINE_CHANNEL_SECRET', None)
 channel_access_token = os.getenv('LINE_CHANNEL_ACCESS_TOKEN', None)
 if channel_secret is None:
@@ -39,17 +36,14 @@ parser = WebhookParser(channel_secret)
 def callback():
     signature = request.headers['X-Line-Signature']
 
-    # get request body as text
     body = request.get_data(as_text=True)
     app.logger.info("Request body: " + body)
 
-    # parse webhook body
     try:
         events = parser.parse(body, signature)
     except InvalidSignatureError:
         abort(400)
 
-    # if event is MessageEvent and message is TextMessage, then echo text
     for event in events:
         if not isinstance(event, MessageEvent):
             continue
@@ -64,10 +58,12 @@ def callback():
 
 
         if event.message.text=="我要買東西":
-        	line_bot_api.push_message(
-                userid,
-                TextSendMessage(text="請問想買甚麼東西呢?(請輸入編號:)")
-            )
+        	line_bot_api.reply_message(
+            event.reply_token,
+            TextSendMessage(text="想買甚麼東西呢?")
+        )
+        	user_want=event.message.text
+        	print(user_want)
 
 
         return 'OK'
