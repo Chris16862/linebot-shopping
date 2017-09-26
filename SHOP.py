3be37d9e78b754234392743268507bb019e30b8a
unknown <>
2017-09-26 20:47:16 +0800

    test

--- SHOP.py
+++ SHOP.py
@@ -53,7 +53,7 @@ def callback():
         if event.message.text=="我要賣東西":
            line_bot_api.reply_message(
             event.reply_token,
-            TextSendMessage(text="請輸入你要賣的東西(依照以下範例):\n"+"依序打入:商品名 規格 價格 介紹\n"+"範例:蘋果 200g 20元 新鮮甜美的蘋果喔!!")
+            TextSendMessage(text="請輸入你要賣的東西(依照以下範例):\n依序打入:商品名 規格 價格 介紹\n範例:蘋果 200g 20元 新鮮甜美的蘋果喔!!")
         	)
         com_info=event.message.text
         print(com_info)
