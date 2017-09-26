62ad3d82888c1d982959bb0059903db5a08f2e7f
unknown <>
2017-09-26 21:39:30 +0800

    test

--- SHOP.py
+++ SHOP.py
@@ -79,36 +79,6 @@ def callback():
                 )
             )
     line_bot_api.reply_message(event.reply_token, buttons_template)
-
-
-
-
-
-
-
-
-
-
-
-        ''' line_bot_api.reply_message(
-                event.reply_token,
-                TextSendMessage(text="請輸入商品名:")
-            )   
-       
-        if event.message.text=="我要賣東西":
-            line_bot_api.reply_message(
-                event.reply_token,
-                TextSendMessage(text="請輸入你要賣的東西(依照以下範例):\n依序打入:商品名 規格 價格 介紹\n範例:蘋果 200g 20元 新鮮甜美的蘋果喔!!")
-            )
-        com_info=event.message.text
-        print(com_info)
-
-        if event.message.text=="我要買東西":
-            line_bot_api.reply_message(
-                event.reply_token,
-                TextSendMessage(text="請輸入商品名:")
-            )
-        if event.message.text'''
     return 'OK'
    
 
