a125a7e73c166b5ca9190f31dbe7331ff45ae5be
unknown <>
2017-09-27 02:23:37 +0800

    test

--- SHOP.py
+++ SHOP.py
@@ -49,8 +49,6 @@ def callback():
             continue
         if not isinstance(event.message, TextMessage):
             continue
-        f = open('set.txt','r',encoding = 'UTF-8')
-        tags=f.read()
         if event.message.text=="我要賣東西":
             line_bot_api.reply_message(
             event.reply_token,
@@ -58,6 +56,8 @@ def callback():
             )
             f = open('set.txt','w',encoding = 'UTF-8')
             f.write("1")
+        f = open('set.txt','r',encoding = 'UTF-8')
+        tags=f.read()
         print(tags)
         if tags=="1":
             mo_name.append(event.message.text)
