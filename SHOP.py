3de2abc40b983a3f9c819ef859a7a8cadad2101b
unknown <>
2017-09-26 21:23:53 +0800

    test

--- SHOP.py
+++ SHOP.py
@@ -1,6 +1,8 @@
 from __future__ import unicode_literals
 
 #import jieba
+from pandas import Series, DataFrame
+import pandas as pd 
 import os
 import sys
 from argparse import ArgumentParser
@@ -49,14 +51,64 @@ def callback():
             continue
         if not isinstance(event.message, TextMessage):
             continue
-   
-        if event.message.text=="我要賣東西":
+   		if event.message.text=="我要賣東西":
+   			  buttons_template = TemplateSendMessage(
+            alt_text='開始玩 template',
+            template=ButtonsTemplate(
+                title='選擇類型',
+                text='請選擇',
+                thumbnail_image_url='https://i.imgur.com/xQF5dZT.jpg',
+                actions=[
+                    MessageTemplateAction(
+                        label='商品名',
+                        text='商品名'
+                    ),
+                    MessageTemplateAction(
+                        label='價錢',
+                        text='價錢'
+                    ),
+                    MessageTemplateAction(
+                        label='規格',
+                        text='規格'
+                    ),
+                    MessageTemplateAction(
+                        label='介紹',
+                        text='介紹'
+                    )
+                ]
+            )
+        )
+	line_bot_api.reply_message(event.reply_token, buttons_template)
+
+
+
+
+
+
+
+
+
+
+
+        	line_bot_api.reply_message(
+            	event.reply_token,
+            	TextSendMessage(text="請輸入商品名:")
+        	)	
+       
+        '''if event.message.text=="我要賣東西":
         	line_bot_api.reply_message(
             	event.reply_token,
             	TextSendMessage(text="請輸入你要賣的東西(依照以下範例):\n依序打入:商品名 規格 價格 介紹\n範例:蘋果 200g 20元 新鮮甜美的蘋果喔!!")
         	)
         com_info=event.message.text
         print(com_info)
+
+        if event.message.text=="我要買東西":
+        	line_bot_api.reply_message(
+            	event.reply_token,
+            	TextSendMessage(text="請輸入商品名:")
+        	)
+        if event.message.text'''
     return 'OK'
    
 
