0d3ec842be6504e9c6ce30dbe9828263547b6a28
Apie0419 <xu3mp6xjp6@gmail.com>
2017-09-28 02:40:19 +0800

    test

--- SHOP.py
+++ SHOP.py
@@ -54,7 +54,6 @@ def callback():
         elif status[0][0]=="enter_name":
             s = "enter_price"
             SQL = "UPDATE sell_list SET name='{}',status='{}' WHERE status='enter_name' and userid='{}';".format(event.message.text, s, userid)
-            print (SQL)
             db.execute(SQL)
             con.commit()
             line_bot_api.reply_message(
@@ -81,15 +80,115 @@ def callback():
             s = "modify"
             db.execute("SELECT name,price,amount FROM sell_list WHERE status='enter_intro' and userid='{}'".format(userid))
             data = db.fetchall()
-            db.execute("UPDATE sell_list SET intro='{}',status='{}' WHERE status='enter_amount' and userid='{}'".format(event.message.text, s, userid))
+            db.execute("UPDATE sell_list SET intro='{}',status='{}' WHERE status='enter_intro' and userid='{}'".format(event.message.text, s, userid))
             con.commit()
-            print (data)
             line_bot_api.reply_message(
             event.reply_token,
             TemplateSendMessage(
                 alt_text='Confirm template',
                 template=ConfirmTemplate(
-                    text="輸入完畢，請確認內容是否需要更改\n商品名:"+data[0][0]+"\n價錢:"+str(data[0][1])+"\n數量:"+str(data[0][2])+"\n介紹及優惠:"+event.message.text,
+                    text="輸入完畢，請確認內容是否需要更改\n商品名:"+data[0][0]+"\n單價:"+str(data[0][1])+"\n數量:"+str(data[0][2])+"\n介紹及優惠:"+event.message.text,
+                    actions=[
+                    MessageTemplateAction(
+                        label='Yes',
+                        text='Yes',
+                        ),
+                    MessageTemplateAction(
+                        label='No',
+                        text='No'
+                        )
+                    ]
+                    )
+                )
+            )
+        elif status[0][0]=="modify_name":
+            db.execute("SELECT price,amount,intro FROM sell_list WHERE status='enter_intro' and userid='{}'".format(userid))
+            data = db.fetchall()
+            s = "modify"
+            SQL = "UPDATE sell_list SET name='{}',status='{}' WHERE status='enter_name' and userid='{}';".format(event.message.text, s, userid)
+            db.execute(SQL)
+            con.commit()
+            line_bot_api.reply_message(
+            event.reply_token,
+            TemplateSendMessage(
+                alt_text='Confirm template',
+                template=ConfirmTemplate(
+                    text="輸入完畢，請確認內容是否需要更改\n商品名:"+event.message.text+"\n單價:"+str(data[0][0])+"\n數量:"+str(data[0][1])+"\n介紹及優惠:"+data[0][2],
+                    actions=[
+                    MessageTemplateAction(
+                        label='Yes',
+                        text='Yes',
+                        ),
+                    MessageTemplateAction(
+                        label='No',
+                        text='No'
+                        )
+                    ]
+                    )
+                )
+            )
+        elif status[0][0]=="modify_price":
+            db.execute("SELECT name,amount,intro FROM sell_list WHERE status='enter_intro' and userid='{}'".format(userid))
+            data = db.fetchall()
+            s = "modify"
+            db.execute("UPDATE sell_list SET price={},status='{}' WHERE status='enter_price' and userid='{}'".format(int(event.message.text), s, userid))
+            con.commit()
+            line_bot_api.reply_message(
+            event.reply_token,
+            TemplateSendMessage(
+                alt_text='Confirm template',
+                template=ConfirmTemplate(
+                    text="輸入完畢，請確認內容是否需要更改\n商品名:"+data[0][0]+"\n單價:"+event.message.text+"\n數量:"+str(data[0][1])+"\n介紹及優惠:"+data[0][2],
+                    actions=[
+                    MessageTemplateAction(
+                        label='Yes',
+                        text='Yes',
+                        ),
+                    MessageTemplateAction(
+                        label='No',
+                        text='No'
+                        )
+                    ]
+                    )
+                )
+            )
+        elif status[0][0]=="modify_amount":
+            db.execute("SELECT name,price,intro FROM sell_list WHERE status='enter_intro' and userid='{}'".format(userid))
+            data = db.fetchall()
+            s = "modify"
+            db.execute("UPDATE sell_list SET amount={},status='{}' WHERE status='enter_amount' and userid='{}'".format(int(event.message.text), s, userid))
+            con.commit()
+            line_bot_api.reply_message(
+            event.reply_token,
+            TemplateSendMessage(
+                alt_text='Confirm template',
+                template=ConfirmTemplate(
+                    text="輸入完畢，請確認內容是否需要更改\n商品名:"+data[0][0]+"\n單價:"+str(data[0][1])+"\n數量:"+event.message.text+"\n介紹及優惠:"+data[0][2],
+                    actions=[
+                    MessageTemplateAction(
+                        label='Yes',
+                        text='Yes',
+                        ),
+                    MessageTemplateAction(
+                        label='No',
+                        text='No'
+                        )
+                    ]
+                    )
+                )
+            )
+        elif status[0][0]=="modify_intro":
+            db.execute("SELECT name,price,amount FROM sell_list WHERE status='enter_intro' and userid='{}'".format(userid))
+            data = db.fetchall()
+            s = "modify"
+            db.execute("UPDATE sell_list SET intro={},status='{}' WHERE status='enter_amount' and userid='{}'".format(event.message.text, s, userid))
+            con.commit()
+            line_bot_api.reply_message(
+            event.reply_token,
+            TemplateSendMessage(
+                alt_text='Confirm template',
+                template=ConfirmTemplate(
+                    text="輸入完畢，請確認內容是否需要更改\n商品名:"+data[0][0]+"\n單價:"+str(data[0][1])+"\n數量:"+data[0][2]+"\n介紹及優惠:"+event.message.text,
                     actions=[
                     MessageTemplateAction(
                         label='Yes',
@@ -104,7 +203,64 @@ def callback():
                 )
             )
         elif status[0][0]=="modify" :
-            pass
+            if event.message.text=='Yes' : 
+                line_bot_api.reply_message(
+                    event.reply_token,
+                    ButtonsTemplate(
+                        title='List',
+                        text='請問需要更改哪個項目？',
+                        actions=[
+                        MessageTemplateAction(
+                            label='商品名',
+                            text='商品名',
+                            ),
+                        MessageTemplateAction(
+                            label='單價',
+                            text='單價'
+                            ),
+                        MessageTemplateAction(
+                            label='數量',
+                            text='數量'
+                            )
+                        MessageTemplateAction(
+                            label='介紹及優惠',
+                            text='介紹及優惠'
+                            )
+                        ]
+                        )
+                    )
+            elif event.message.text=='No' :
+                s = 'finish'
+                db.execute("UPDATE sell_list SET status='{}' WHERE status='enter_intro' and userid='{}'".format(s, userid))
+                con.commit()
+                line_bot_api.reply_message(
+                event.reply_token,
+                TextSendMessage(text="商品新增完成")
+                )
+            elif event.message.text=="商品名" :
+                s = "modify_name"
+                line_bot_api.reply_message(
+                event.reply_token,
+                TextSendMessage(text="請輸入商品名:")
+                )
+            elif event.message.text=="單價" :
+                s = "modify_price"
+                line_bot_api.reply_message(
+                event.reply_token,
+                TextSendMessage(text="請輸入單價:")
+                )
+            elif event.message.text=="數量" :
+                s = "modify_amount"
+                line_bot_api.reply_message(
+                event.reply_token,
+                TextSendMessage(text="請輸入數量:")
+                )
+            elif event.message.text=="介紹及優惠":
+                s = "modify_intro"
+                line_bot_api.reply_message(
+                event.reply_token,
+                TextSendMessage(text="請輸入介紹及優惠:")
+                )
     return 'OK'
    
 
