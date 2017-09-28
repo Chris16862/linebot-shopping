f5b7297627de91f2c2fc35b5b0c164ec364b91af
Apie0419 <xu3mp6xjp6@gmail.com>
2017-09-29 00:59:17 +0800

    test

--- SHOP.py
+++ SHOP.py
@@ -246,16 +246,19 @@ def callback():
                 s = 'finish'
                 db.execute("SELECT * FROM group_list")
                 ids = db.fetchall()
-                db.execute("SELECT intro,id FROM sell_list WHERE userid='{}' and status='modify'".format(userid))
+                db.execute("SELECT intro,id,name,amount,price FROM sell_list WHERE userid='{}' and status='modify'".format(userid))
                 data = db.fetchall()
                 intro = data[0][0]
                 number = data[0][1]
+                name = data[0][2]
+                amount = data[0][3]
+                price = data[0][4]
                 db.execute("UPDATE sell_list SET status='{}' WHERE status='modify' and userid='{}'".format(s, userid))
                 con.commit()
                 for i in ids :
                     line_bot_api.push_message(
                     i[0],
-                    TextSendMessage(text="商品標號#"+str(number)+"\n"+intro)
+                    TextSendMessage(text="商品標號#"+str(number)+"\n商品名:"+name+"\n單價:"+str(price)+"\n數量:"+str(amount)+"\n"+intro+"\n如需購買請私訊我喔～")
                     )
             elif event.message.text=="商品名" :
                 s = "modify_name"
